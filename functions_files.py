1f6433478b92068a247ce5c3837b3a4ca193f603
s <salehahr@users.noreply.github.com>
2021-10-30 23:15:28 +0200

    added more functions to manipulate files

--- functions_files.py
+++ functions_files.py
@@ -1,9 +1,27 @@
 import os
+import shutil
 
 
 def make_folder(folder_name: str) -> None:
-    path = os.path.join(os.getcwd(), folder_name)
+    path = os.path.abspath(folder_name)
     try:
-        os.mkdir(path)
+        os.makedirs(path)
     except FileExistsError:
         pass
+
+
+def make_folders(list_of_folders: list):
+    for f in list_of_folders:
+        make_folder(f)
+
+
+def remove_data_folders(path):
+    try:
+        shutil.rmtree(path)
+    except FileNotFoundError:
+        pass
+
+
+def delete_files(list_of_files):
+    for f in list_of_files:
+        os.remove(f)
