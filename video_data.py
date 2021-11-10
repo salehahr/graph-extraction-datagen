2bfadd379188b5879d27e7876b8f332062323727
s <salehahr@users.noreply.github.com>
2021-11-10 10:54:22 +0100

    grk008 trim times

--- video_data.py
+++ video_data.py
@@ -1,18 +1,14 @@
-
-
 # Video
 # VIDEO_FULL_FILEPATH_EXT = 'M:/graph-training/data/GRK008.mp4'
-VIDEO_FULL_FILEPATH_EXT = '/graphics/scratch/schuelej/sar/data/GRK008.mp4'
-frequency = 25 # Hz
-trim_times_in_s = None
-# trim_times_in_s = [[0, 1], [2, 3]]
-
+VIDEO_FULL_FILEPATH_EXT = '/graphics/scratch/schuelej/sar/data/GRK008.MP4'
+frequency = 25  # Hz
+trim_times_in_s = [[0, 9.6], [15, 16.2],
+                   [26.1, 26.8], [28.1, 29.2], [29.8, 29.8],
+                   [32.4, 38.4], [39.4, 40.32],
+                   ]
 
 # Image Dimensions
-# vid_height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
 img_length = 512
-
 
 # Plot/Save options
 thr_plot = False
