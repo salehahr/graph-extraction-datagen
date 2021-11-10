07263de41d88cd740017662eb6c1dbf06a6facb8
s <salehahr@users.noreply.github.com>
2021-11-10 19:56:24 +0100

    functions for extracting extended adjacency matrix

--- functions/graphs.py
+++ functions/graphs.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import numpy as np
 
 from functions.im2graph import node_extraction, edge_extraction
 
@@ -9,7 +10,28 @@ def get_positions_list(graph) -> list:
     return [xy for xy in pos_dict.values()]
 
 
+def get_positions_vector(graph) -> np.ndarray:
+    pos_list = get_positions_list(graph)
+
+    positions_vector = np.zeros((len(graph), 2))
+    for i, xy in enumerate(pos_list):
+        positions_vector[i, :] = xy
+    return positions_vector
+
+
 def extract_nodes_edges(img_preproc, node_size):
     bcnodes, _, endpoints, _, _, allnodescoor, marked_img = node_extraction(img_preproc, node_size)
     _, esecoor, _, coordinates_global = edge_extraction(img_preproc, endpoints, bcnodes)
     return allnodescoor, coordinates_global, esecoor, marked_img
+
+
+def get_adjacency_matrix(graph, nodelist=None, do_save: bool = False, filepath:str = ''):
+    adj_matrix = nx.convert_matrix.to_numpy_matrix(graph, nodelist=nodelist)
+    positions_vector = get_positions_vector(graph)
+
+    extended_adj_matrix = np.hstack([positions_vector, adj_matrix])
+
+    if do_save and filepath:
+        np.save(filepath, extended_adj_matrix)
+
+    return extended_adj_matrix
--- tests/test_images.py
+++ tests/test_images.py
@@ -3,9 +3,11 @@ import unittest
 
 import cv2
 import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
 
 from config import Config
-from functions.graphs import get_position
+from functions.graphs import get_positions_list, get_adjacency_matrix
 from functions.images import crop_resize_square, is_square, crop_radius, generate_node_pos_img
 from functions.images import get_rgb, get_centre
 from functions.images import extract_graph_and_helpers
@@ -29,6 +31,17 @@ class TestShortVideoImage(unittest.TestCase):
 
         first_img_fp = cls.config.raw_image_files[0]
         cls.img = cv2.imread(first_img_fp, cv2.IMREAD_COLOR)
+
+        img_skel_fp = cls.config.skeletonised_image_files[0]
+        cls.img_skel = cv2.imread(img_skel_fp, cv2.IMREAD_GRAYSCALE)
+
+        cls.graph = cls.get_graph()
+
+    @classmethod
+    def get_graph(cls):
+        cls.config.lm_save = False
+        graph, _, _, _ = extract_graph_and_helpers(cls.config, cls.img_skel, '')
+        return graph
 
     def test_trimmed(self):
         self.assertFalse(self.config.has_trimmed)
@@ -59,19 +72,42 @@ class TestShortVideoImage(unittest.TestCase):
         self.assertEqual(self.config.img_length, cr_height)
         self.assertEqual(self.config.img_length, cr_width)
 
-    def test_extract_graph(self):
-        img_skel_fp = self.config.skeletonised_image_files[0]
-        img_skel = cv2.imread(img_skel_fp, cv2.IMREAD_GRAYSCALE)
-        self.config.lm_save = False
+    def test_generate_node_pos_img(self):
+        self.assertIsNotNone(self.graph)
 
-        graph, _, _, _ = extract_graph_and_helpers(self.config, img_skel, '')
-        pos = get_position(graph)
+        pos = get_positions_list(self.graph)
+        self.assertIsNotNone(pos)
+
         node_pos_img = generate_node_pos_img(self.config, pos)
 
         temp_fp_skeleton = os.path.join(base_path, 'temp_skel.png')
         temp_fp_nodepos = os.path.join(base_path, 'temp_nodepos.png')
-        cv2.imwrite(temp_fp_skeleton, img_skel)
+        cv2.imwrite(temp_fp_skeleton, self.img_skel)
         cv2.imwrite(temp_fp_nodepos, node_pos_img)
 
-        self.assertIsNotNone(graph)
-        self.assertIsNotNone(pos)
+    def test_get_adjacency_matrix(self):
+        fp_adj_matrix = os.path.join(base_path, 'adj_matr.npy')
+        extended_adj_matrix = get_adjacency_matrix(self.graph,
+                                                   do_save=True,
+                                                   filepath=fp_adj_matrix)
+
+        self.assertTrue(os.path.isfile(fp_adj_matrix))
+
+        with open(fp_adj_matrix, 'rb') as f:
+            a = np.load(f)
+            self.assertEqual(a.shape[0], len(self.graph))
+            self.assertEqual(a.shape[1], 2 + len(self.graph))
+
+        os.remove(fp_adj_matrix)
+        self.assertFalse(os.path.isfile(fp_adj_matrix))
+
+    def test_adjacency_matrix(self):
+        """ Test adjacency matrix with and without nodelist. """
+        nodelist = sorted(self.graph.nodes())
+
+        adj_matrix = nx.convert_matrix.to_numpy_matrix(self.graph)
+        adj_matrix_nl = nx.convert_matrix.to_numpy_matrix(self.graph,
+                                                          nodelist=nodelist)
+
+        np.testing.assert_equal(adj_matrix, adj_matrix_nl)
+        self.assertEqual(adj_matrix.shape[1], adj_matrix.shape[0])
