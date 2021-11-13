eaeca2532f4664850e772a30cf07a48dfb1036e8
s <salehahr@users.noreply.github.com>
2021-11-13 18:00:34 +0100

    extended test_graph for types: generated, read

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,7 +1,6 @@
 import os
 
 import cv2
-import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,19 +13,19 @@ from tools.plots import plot_graph_on_img_straight
 
 
 class TestGraph(TestVideoFrame):
+    """
+    Sanity checks:
+        * tests whether the node positions are sorted
+        * tests whether the adjacency matrix matches the skeletonised image
+    """
     @classmethod
     def setUpClass(cls) -> None:
         super(TestGraph, cls).setUpClass()
 
-        cls.fp_adj_matrix = os.path.join(test_data_path, 'adj_matr.npy')
-        if os.path.isfile(cls.fp_adj_matrix):
-            os.remove(cls.fp_adj_matrix)
-
         cls.img_skel = cv2.imread(cls.img_skeletonised_fp, cv2.IMREAD_GRAYSCALE)
         cls.plot_skeletonised()
 
-        cls.graph, _, _, _ = extract_graph_and_helpers(cls.img_skel, '')
-        cls.pos_list = get_positions_list(cls.graph)
+        cls.pos_list = None
 
     @classmethod
     def plot_skeletonised(cls):
@@ -34,25 +33,69 @@ class TestGraph(TestVideoFrame):
         plt.title(os.path.relpath(cls.img_skeletonised_fp, start=cls.base_path))
         plt.show()
 
-    def test_generate_node_pos_img(self):
-        node_pos_img = generate_node_pos_img(self.graph, img_length)
-
-        plot_img(node_pos_img)
-        plt.show()
+    def plot_adj_matr(self, adj_matr):
+        plot_graph_on_img_straight(self.img_skel, self.pos_list, adj_matr)
 
     def test_is_pos_list_sorted(self):
-        row = [x for x, _ in self.pos_list]
-        col = [y for _, y in self.pos_list]
-
+        rows, cols = zip(*self.pos_list)
         print(self.pos_list)
 
         def is_sorted_ascending(arr):
             return all(arr[i] <= arr[i + 1] for i in range(len(arr) - 1))
 
-        is_sorted_row = is_sorted_ascending(row)
-        is_sorted_col = is_sorted_ascending(col)
+        is_sorted_row = is_sorted_ascending(rows)
+        is_sorted_col = is_sorted_ascending(cols)
 
         self.assertTrue(is_sorted_row or is_sorted_col)
+
+    def test_adjacency_matrix_skeletonised_match(self):
+        pass
+
+
+class TestReadGraph(TestGraph):
+    """
+    Implements TestGraph checks for pre-generated (already saved)
+    adjacency matrix and node positions of a random image.
+    """
+    @classmethod
+    def setUpClass(cls) -> None:
+        super(TestReadGraph, cls).setUpClass()
+
+        fp = cls.img_skeletonised_fp
+        node_pos_vec_fp = os.path.splitext(fp.replace('skeleton', 'node_positions'))[0] + '.npy'
+        adj_matr_fp = os.path.splitext(fp.replace('skeleton', 'adj_matr'))[0] + '.npy'
+
+        ext_adj_matr = np.load(adj_matr_fp)
+        cls.adj_matr = ext_adj_matr[0, :, 2:].astype(np.uint8)
+
+        node_pos = np.load(node_pos_vec_fp).astype(np.uint8)
+        cls.pos_list = np.ndarray.tolist(node_pos)
+
+    def test_adjacency_matrix_skeletonised_match(self):
+        self.plot_adj_matr(self.adj_matr)
+
+
+class TestGenerateGraph(TestGraph):
+    """
+    Implements TestGraph checks for
+    adjacency matrix and node positions which are generated on the fly.
+    """
+    @classmethod
+    def setUpClass(cls) -> None:
+        super(TestGenerateGraph, cls).setUpClass()
+
+        cls.fp_adj_matrix = os.path.join(test_data_path, 'adj_matr.npy')
+        if os.path.isfile(cls.fp_adj_matrix):
+            os.remove(cls.fp_adj_matrix)
+
+        cls.graph, _, _, _ = extract_graph_and_helpers(cls.img_skel, '')
+        cls.pos_list = get_positions_list(cls.graph)
+
+    def test_generate_node_pos_img(self):
+        node_pos_img = generate_node_pos_img(self.graph, img_length)
+
+        plot_img(node_pos_img)
+        plt.show()
 
     def test_adjacency_matrix_skeletonised_match(self):
         ext_adj_matr = get_ext_adjacency_matrix(self.graph,
@@ -61,18 +104,7 @@ class TestGraph(TestVideoFrame):
         self.assertTrue(os.path.isfile(self.fp_adj_matrix))
 
         adj_matr = ext_adj_matr[0, :, 2:]
-        plot_graph_on_img_straight(self.img_skel, self.pos_list, adj_matr)
-
-    def test_adjacency_matrix(self):
-        """ Test adjacency matrix with and without nodelist. """
-        nodelist = sorted(self.graph.nodes())
-
-        adj_matrix = nx.convert_matrix.to_numpy_matrix(self.graph)
-        adj_matrix_nl = nx.convert_matrix.to_numpy_matrix(self.graph,
-                                                          nodelist=nodelist)
-
-        np.testing.assert_equal(adj_matrix, adj_matrix_nl)
-        self.assertEqual(adj_matrix.shape[1], adj_matrix.shape[0])
+        self.plot_adj_matr(adj_matr)
 
     @classmethod
     def tearDownClass(cls) -> None:
