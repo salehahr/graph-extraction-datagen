9f364fd6039e3825f334d91114f9e2c339df9833
s <salehahr@users.noreply.github.com>
2021-12-17 16:30:31 +0100

    video data GRK22

--- video_data.py
+++ video_data.py
@@ -20,4 +20,20 @@ video_filepath = "/graphics/scratch/schuelej/sar/data/synthetic-bladder6.mp4"
 trim_times = None
 is_synthetic = True
 
+video_filepath = "/graphics/scratch/schuelej/sar/data/GRK022.MP4"
+trim_times = [
+    # [4, 10],
+    [11, 12],
+    [20, 21],
+    [22, 24],
+    [24.7, 25],
+    [26, 27],
+    [32, 34],
+    [39, 40],
+    # [60 + 15, 60 + 19],
+    [60 + 33, 60 + 34],
+    [60 + 43, 60 + 47],
+]
+is_synthetic = False
+
 frequency = 25  # Hz
